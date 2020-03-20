5d87d91cc3ae86e8bf9ffaf9cfe6d9f5fe0e0dca
Liam Brew <you@example.com>
2020-03-20 13:57:47 -0400

    !help

---
+++ commands/info/help.js
@@ -0,0 +1,60 @@
+const { MessageEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  category: "info",
+  description: "Returns all commands, or one specific command info",
+  usage: "[command | alias]",
+  run: async (client, message, args) => {
+    if (args[0]) return getCMD(client, message, args[0]);
+    else return getAll(client, message);
+  }
+};
+
+function getAll(client, message) {
+  const embed = new MessageEmbed();
+
+  const commands = category => {
+    return client.commands
+      .filter(cmd => cmd.category === category)
+      .map(cmd => `- \`${cmd.name}\``)
+      .join("\n");
+  };
+
+  const info = client.categories
+    .map(
+      cat =>
+        stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(
+          cat
+        )}`
+    )
+    .reduce((string, categort) => string + "\n" + categort);
+
+  return message.channel.send(embed.setDescription(info));
+}
+
+function getCMD(client, message, input) {
+  const embed = new MessageEmbed();
+
+  const cmd =
+    client.commands.get(input.toLowerCase()) ||
+    client.commands.get(client.aliases.get(input.toLowerCase()));
+
+  let info = `No information found for command **${input.toLowerCase()}**`;
+
+  if (!cmd)
+    return message.channel.send(embed.setColor("ff0000").setDescription(info));
+
+  if (cmd.name) info = `**Command name** ${cmd.name}`;
+  if (cmd.aliases)
+    info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
+  if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+  if (cmd.usage) {
+    info += `\n**Usage**: ${cmd.usage}`;
+    embed.setFooter(`Syntax: <> = required, [] - optional`);
+  }
+
+  return message.channel.send(embed.setColor("GREEN").setDescription(info));
+}
