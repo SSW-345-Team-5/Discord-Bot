4d99271457677a80af80397ccbde84538ce75ed4
Liam Brew <you@example.com>
2020-03-20 14:02:10 -0400

    !ping

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,6 +4,6 @@ module.exports = {
     description: "Returns latency and API ping", 
     run: async (client, message, args) => {
         const msg = await message.channel.send("Pinging...");
-        msg.edit(`Ping! \nLatency is ${Math.floor(msg.createdAt - message.createdAt)}ms\nAPI Latency is ${Math.round(cient.ping)}ms`);
+        msg.edit(`Ping! \nLatency is ${Math.floor(msg.createdAt - message.createdAt)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
     }
 }
