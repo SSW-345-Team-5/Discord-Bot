9e767046332f144941aa96f49f7f28be8269fc7d
Liam Brew <lbrew@stevens.edu>
2020-03-23 22:35:06 -0400

    update help for new command organziation

--- commands/info/help.js
+++ commands/info/help.js
@@ -6,16 +6,26 @@ module.exports = {
   aliases: ["h"],
   category: "info",
   description: "Returns all commands, or one specific command info",
-  usage: "[command || alias]",
+  usage: "[command | alias]",
   run: async (client, message, args) => {
-    if (args[0]) return getCMD(client, message, args[0]);
-    else return getAll(client, message);
+    // If there's an args found
+    // Send the info of that command found
+    // If no info found, return not found embed.
+    if (args[0]) {
+      return getCMD(client, message, args[0]);
+    } else {
+      // Otherwise send all the commands available
+      // Without the cmd info
+      return getAll(client, message);
+    }
   }
 };
 
 function getAll(client, message) {
-  const embed = new MessageEmbed();
+  const embed = new MessageEmbed().setColor("GREEN");
 
+  // Map all the commands
+  // with the specific category
   const commands = category => {
     return client.commands
       .filter(cmd => cmd.category === category)
@@ -23,10 +33,11 @@ function getAll(client, message) {
       .join("\n");
   };
 
+  // Map all the categories
   const info = client.categories
     .map(
       cat =>
-        stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands}(
+        stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(
           cat
         )}`
     )
@@ -38,22 +49,26 @@ function getAll(client, message) {
 function getCMD(client, message, input) {
   const embed = new MessageEmbed();
 
+  // Get the cmd by the name or alias
   const cmd =
     client.commands.get(input.toLowerCase()) ||
     client.commands.get(client.aliases.get(input.toLowerCase()));
 
   let info = `No information found for command **${input.toLowerCase()}**`;
 
-  if (!cmd)
-    return message.channel.send(embed.setColor("ff0000").setDescription(info));
+  // If no cmd is found, send not found embed
+  if (!cmd) {
+    return message.channel.send(embed.setColor("RED").setDescription(info));
+  }
 
-  if (cmd.name) info = `**Command Name:** ${cmd.name}`;
+  // Add all cmd info to the embed
+  if (cmd.name) info = `**Command name**: ${cmd.name}`;
   if (cmd.aliases)
     info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
   if (cmd.description) info += `\n**Description**: ${cmd.description}`;
   if (cmd.usage) {
     info += `\n**Usage**: ${cmd.usage}`;
-    embed.setFooter(`Syntax: <> = required, [] - optional`);
+    embed.setFooter(`Syntax: <> = required, [] = optional`);
   }
   if (cmd.parameters) {
     info += `\n**Parameters**:`;
@@ -61,7 +76,6 @@ function getCMD(client, message, input) {
       info += `\n${key}: ${cmd.parameters[key]}`;
     }
   }
-
 
   return message.channel.send(embed.setColor("GREEN").setDescription(info));
 }
